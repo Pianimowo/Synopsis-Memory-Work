cb9f0fcc4931809f3f31a0d9975612bf21c97b92
bigmeii <ethan.mei06@gmail.com>
2025-06-14 23:57:47 -0400

    Cleaned up redundancies

--- Modules/OTP.py
+++ Modules/OTP.py
@@ -8,16 +8,19 @@ class OTP:
         self.VDD = VDD
         self.VPP = VPP
         self.VRR = VRR
+        self.multipliers = None
+        self.memory = None
+        self.address_bits = None
 
         # self.grid = [[0 for _ in range(size[0])] for _ in range(size[1])]
 
     def createGrid(self, rows, cols):
         self.memory = np.zeros((rows, cols), dtype=np.float32)
-        self.noise()
+        self.generateNoise()
 
     def loadGrid(self):
         self.memory = np.load("Data/memoryGrid.npy")
-        self.noise()
+        self.generateNoise()
 
     def edit(self, A, D, SEL, WE):
         length = math.log(self.memory.shape[0], 2)
@@ -26,13 +29,10 @@ class OTP:
 
         row = decoder(A[0:int(length)])
         col = decoder(A[int(length):len(A)])
-
         if WE == 1:
             self.write(row, col, D, SEL)
         else:
             self.read(row, col, SEL)
-        
-        np.save("Data/memoryGrid.npy", self.memory)
 
     def write(self, row, col, D, SEL):
         if SEL > 0:
@@ -43,9 +43,12 @@ class OTP:
             out = self.memory[row, col]
             print(compare(out, self.VRR))
 
-    def noise(self):
+    def generateNoise(self):
         self.multipliers = np.random.normal(loc=0.9, scale = 0.05, size = self.memory.shape)
         self.multipliers = np.clip(self.multipliers, 0.0, 1.0)
+
+    def save(self):
+        np.save("Data/memoryGrid.npy", self.memory)
 
     def getGrid(self):
         for row in self.memory:
@@ -54,5 +57,6 @@ class OTP:
 if __name__ == "__main__":
     test = OTP(5, 8, 0.4)
     test.loadGrid()
-    test.edit([0, 1, 1, 1, 1, 0, 0], 1, 1, 1)
+    test.edit([0, 0, 0, 0, 0, 0, 0, 0], 1, 1, 1)
     test.getGrid()
+    test.save()
