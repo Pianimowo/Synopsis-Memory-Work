a951cea12b2f87df346573498b7740634bb65ba4
bigmeii <ethan.mei06@gmail.com>
2025-06-14 22:59:22 -0400

    Integrated numpy for memory

--- Modules/OTP.py
+++ Modules/OTP.py
@@ -4,50 +4,57 @@ from universalFunctions import compare
 from universalFunctions import decoder
 
 class OTP:
-    def __init__(self, size, VDD, VPP, VRR):
+    def __init__(self, VDD, VPP, VRR):
         self.VDD = VDD
         self.VPP = VPP
         self.VRR = VRR
-        self.size = size
 
-        self.len = int(math.log(self.size[0], 2))
-        self.wid = int(math.log(self.size[1], 2))
+        # self.grid = [[0 for _ in range(size[0])] for _ in range(size[1])]
 
-        self.grid = [[0 for _ in range(size[0])] for _ in range(size[1])]
+    def createGrid(self, rows, cols):
+        self.memory = np.zeros((rows, cols), dtype=bool).astype(int)
 
-    # def createGrid(self, A, D, SEL, WE):
+    def loadGrid(self):
+        self.memory = np.load("Data/memoryGrid.npy").astype(int)
 
 
     def edit(self, A, D, SEL, WE):
+        length = math.log(self.memory.shape[0], 2)
+
+        print(length)
 
         D = D*self.VDD
 
-        row = decoder(A[0:int(self.len)])
-        col = decoder(A[int(self.len):len(A)])
+        row = decoder(A[0:int(length)])
+        col = decoder(A[int(length):len(A)])
 
         if WE == 1:
             self.write(row, col, D, SEL)
         else:
             self.read(row, col, SEL)
+        
+        np.save("Data/memoryGrid.npy", self.memory)
 
-        for i in self.grid:
-            print(i)
+
 
     def write(self, row, col, D, SEL):
         if SEL > 0:
-            with open("Data\memoryGrid.txt","w") as f:
-                f.write("write")
-            self.grid[row][col] = D
+            self.memory[row, col] = D
 
     def read(self, row, col, SEL):
         if SEL > 0:
-            out = compare(self.grid[row][col], self.VRR)
+            out = self.memory[row, col]
             print(out)
 
+    def getGrid(self):
+        memoryGrid = np.load("Data/memoryGrid.npy").astype(int)
+        print(memoryGrid)
 
     def test(self):
         print("test")
 
 if __name__ == "__main__":
-    test = OTP([8, 8], 5, 8, 0.4)
-    test.edit([0, 0, 1, 0, 1, 0], 1, 1, 1)
+    test = OTP(5, 8, 0.4)
+    test.loadGrid()
+    test.edit([0, 0, 0, 1, 0, 1, 0], 1, 1, 1)
+    test.getGrid()
